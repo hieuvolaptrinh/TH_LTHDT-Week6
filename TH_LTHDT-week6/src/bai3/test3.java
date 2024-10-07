52f8bcb255c92e4957c37f61dd57c0e3b3d8e452
hieuvolaptrinh <vndhieuak@gmail.com>
2024-10-07 22:40:43 +0700

    hieus làm bt rồi mà quên push

---
+++ TH_LTHDT-week6/src/bai3/test3.java
@@ -0,0 +1,43 @@
+package bai3;
+
+import java.util.Scanner;
+
+public class test3 {
+    public static void main(String[] args) {
+        bai3_TKNganHang tk = new bai3_TKNganHang();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("nhập số tài khoản");
+        tk.setSoTK(sc.nextLine());
+        System.out.println("nhập tên chủ tài khoản");
+        tk.setChuTk(sc.nextLine());
+        System.out.println("nhập số dư");
+        tk.setSoDu(sc.nextLong());
+        int luaChon;
+        do {
+            System.out.println("1. gửi tiền");
+            System.out.println("2. rút tiền");
+            System.out.println("3. kiểm tra số dư");
+            System.out.println("0. thoát");
+            luaChon = sc.nextInt();
+            switch (luaChon){
+                case 1:
+                    System.out.println("nhập số tiền bạn muốn gửi");
+                    long tienGui = sc.nextLong();
+                    tk.guiTien(tienGui);
+                    break;
+                case 2:
+                    System.out.println("nhập số tiền bạn muốn rút");
+                    long tienRut = sc.nextLong();
+                    tk.rutTien(tienRut);
+                    break;
+                case 3:
+                    tk.kiemTraSoDu();
+                    break;
+                default:
+                    System.out.println("bạn đã thoát ");
+                    break;
+            }
+
+        }while (luaChon != 0);
+    }
+}
